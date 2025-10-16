8c1278489cd89f36662a808d964e96081c644282
Konrad Kohbrok <konrad.kohbrok@datashrine.de>
2025-10-16 17:00:43 +0200

    nit

--- apiclient/src/ds_api/grpc.rs
+++ apiclient/src/ds_api/grpc.rs
@@ -82,7 +82,7 @@ impl DsGrpcClient {
             group_state_ear_key: Some(group_state_ear_key.ref_into()),
             message: Some(params.message.try_ref_into()?),
             sender: Some(params.sender.into()),
-            suppress_notifications: params.suppress_notifications,
+            suppress_notifications: Some(params.suppress_notifications),
         };
 
         let request = payload.sign(signing_key)?;
