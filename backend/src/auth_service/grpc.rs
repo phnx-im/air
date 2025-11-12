phnx-im/air

commit 850b67c1ab30a842f6c870a06efd2af8c302e99d

fix(server): suppress disconnect errors in queues (#808)
When a client diconnects from the username queues or from the QS queue,
an error is raised in the backend with the underlying cause of broken
pipe. This is not an error, so we do not report it anymore.

backend/src/auth_service/grpc.rs
@@ -2,6 +2,8 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+use std::io;
+
 use airprotos::{
     auth_service::v1::{auth_service_server, *},
     validation::MissingFieldExt,
@@ -31,8 +33,10 @@ use privacypass::{amortized_tokens::AmortizedBatchTokenRequest, private_tokens::
 use tls_codec::{Deserialize, Serialize};
 use tokio::sync::mpsc;
 use tokio_stream::StreamExt;
-use tonic::{Request, Response, Status, Streaming, async_trait};
+use tonic::{Code, Request, Response, Status, Streaming, async_trait};
 use tracing::error;
+
+use crate::util::find_cause;
 
 use super::{
     AuthService,
@@ -46,6 +50,7 @@ pub struct GrpcAs {
 
 impl GrpcAs {
     pub fn new(inner: AuthService) -> Self {
+        tracing::debug!("new");
         Self { inner }
     }
 
@@ -124,6 +129,14 @@ impl GrpcAs {
     ) {
         while let Some(request) = requests.next().await {
             if let Err(error) = Self::process_listen_handle_request(&queues, request).await {
+                if let Code::Unknown = error.code()
+                    && let Some(h2_error) = find_cause::<h2::Error>(&error)
+                    && let Some(io_error) = h2_error.get_io()
+                    && io_error.kind() == io::ErrorKind::BrokenPipe
+                {
+                    return; // Client closed connection => not an error
+                }
+
                 // We report the error, but don't stop processing requests.
                 // TODO(#466): Send this to the client.
                 error!(%error, "error processing listen request");

backend/src/qs/grpc.rs
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-use std::pin::Pin;
+use std::{io, pin::Pin};
 
 use airprotos::{
     queue_service::v1::{queue_service_server::QueueService, *},
@@ -20,12 +20,13 @@ use aircommon::{
 };
 use displaydoc::Display;
 use tokio_stream::{Stream, StreamExt};
-use tonic::{Request, Response, Status, Streaming, async_trait};
+use tonic::{Code, Request, Response, Status, Streaming, async_trait};
 use tracing::error;
 
 use crate::{
     errors::QueueError,
     qs::{client_record::QsClientRecord, queue::Queues, user_record::UserRecord},
+    util::find_cause,
 };
 
 use super::Qs;
@@ -47,6 +48,15 @@ impl GrpcQs {
         while let Some(request) = requests.next().await {
             if let Err(error) = Self::process_listen_queue_request(&queues, queue_id, request).await
             {
+                if let ProcessListenQueueRequestError::Status(error) = &error
+                    && let Code::Unknown = error.code()
+                    && let Some(h2_error) = find_cause::<h2::Error>(&error)
+                    && let Some(io_error) = h2_error.get_io()
+                    && io_error.kind() == io::ErrorKind::BrokenPipe
+                {
+                    return; // Client closed connection => not an error
+                }
+
                 // We report the error, but don't stop processing requests.
                 // TODO(#466): Send this to the client.
                 error!(%error, "error processing listen queue request");

Cargo.toml
@@ -50,6 +50,7 @@ enumset = "1.1.5"
 flate2 = "1.1.0"
 flutter_rust_bridge = { version = "=2.11.1", features = ["chrono", "uuid"] }
 futures-util = "0.3.31"
+h2 = "0.4.12"
 hex = "0.4.3"
 hkdf = "0.12"
 image = "0.25.1"

backend/Cargo.toml
@@ -23,6 +23,7 @@ chrono.workspace = true
 dashmap.workspace = true
 displaydoc.workspace = true
 futures-util.workspace = true
+h2.workspace = true
 metrics.workspace = true
 mimi-room-policy.workspace = true
 mls-assist.workspace = true

backend/src/lib.rs
@@ -15,5 +15,6 @@ pub(crate) mod pg_listen;
 pub mod qs;
 pub mod rate_limiter;
 pub mod settings;
+pub(crate) mod util;
 
 pub use mls_assist::messages::{AssistedGroupInfo, AssistedMessageOut};

backend/src/util.rs
@@ -1,0 +1,17 @@
+// SPDX-FileCopyrightText: 2025 Phoenix R&D GmbH <hello@phnx.im>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+/// Find the first error in the source chain that is of type `T`.
+pub(crate) fn find_cause<T: std::error::Error + 'static>(
+    error: &dyn std::error::Error,
+) -> Option<&T> {
+    let mut source = error.source();
+    while let Some(error) = source {
+        if let Some(typed) = error.downcast_ref() {
+            return Some(typed);
+        }
+        source = error.source();
+    }
+    None
+}

commit 13f2c1e90db6a79460780e07ceb263faea9e4c82

hotfix(server): stale qs listeners are not cleaned up
This is a regression introduced in #808. Previously, we stopped tracking
a queue listener when we got an error in the incoming requests handler
in QS. When error handling was suppressed, we stopped calling `untrack
listener`, which is the bug. As a consequence, no push notifications
were sent to the client because it was considered as connected to the
server.

However, it is better to tie the listener's liveness to the lifetime of
the queue context, which is owned by the queue listen stream. Now, when
gRPC drops the stream, the context is dropped and cancels the listener.
It also unsubscribes from the Postgres notifications.

Also, debug logs have been added to the queue listener and PG listener
task.

backend/src/auth_service/grpc.rs
@@ -50,7 +50,6 @@ pub struct GrpcAs {
 
 impl GrpcAs {
     pub fn new(inner: AuthService) -> Self {
-        tracing::debug!("new");
         Self { inner }
     }
 
@@ -134,12 +133,12 @@ impl GrpcAs {
                     && let Some(io_error) = h2_error.get_io()
                     && io_error.kind() == io::ErrorKind::BrokenPipe
                 {
-                    return; // Client closed connection => not an error
+                    // Client closed connection => not an error
+                } else {
+                    // We report the error, but don't stop processing requests.
+                    // TODO(#466): Send this to the client.
+                    error!(%error, "error processing listen request");
                 }
-
-                // We report the error, but don't stop processing requests.
-                // TODO(#466): Send this to the client.
-                error!(%error, "error processing listen request");
             }
         }
     }

backend/src/auth_service/user_handles/queue.rs
@@ -115,13 +115,13 @@ impl UserHandleQueues {
     async fn track_listener(&self, hash: UserHandleHash) -> sqlx::Result<CancellationToken> {
         let mut listeners = self.listeners.lock().await;
         for (hash, _) in listeners.extract_if(|_, cancel| cancel.is_cancelled()) {
-            self.pg_listener_task_handle.unlisten(hash).await;
+            self.pg_listener_task_handle.unlisten(hash);
         }
         let cancel = CancellationToken::new();
         if let Some(prev_cancel) = listeners.insert(hash, cancel.clone()) {
             prev_cancel.cancel();
         } else {
-            self.pg_listener_task_handle.listen(hash).await;
+            self.pg_listener_task_handle.listen(hash);
         }
         Ok(cancel)
     }

backend/src/pg_listen.rs
@@ -10,7 +10,7 @@ use sqlx::postgres::{PgListener, PgNotification};
 use thiserror::Error;
 use tokio::sync::{broadcast, mpsc};
 use tokio_stream::StreamExt;
-use tracing::{error, info};
+use tracing::{debug, error, info};
 
 /// A handle to a running [`PgListener`] task.
 ///
@@ -18,18 +18,18 @@ use tracing::{error, info};
 #[derive(Debug, Clone)]
 pub(crate) struct PgListenerTaskHandle<C> {
     broadcast: broadcast::Sender<C>,
-    listener_tx: mpsc::Sender<Command<C>>,
+    listener_tx: mpsc::UnboundedSender<Command<C>>,
 }
 
 impl<C: PgChannelName> PgListenerTaskHandle<C> {
-    pub(crate) async fn listen(&self, channel: C) {
-        if let Err(error) = self.listener_tx.send(Command::Listen(channel)).await {
+    pub(crate) fn listen(&self, channel: C) {
+        if let Err(error) = self.listener_tx.send(Command::Listen(channel)) {
             error!(%error, "Error sending listen command to pg listener task");
         }
     }
 
-    pub(crate) async fn unlisten(&self, channel: C) {
-        if let Err(error) = self.listener_tx.send(Command::Unlisten(channel)).await {
+    pub(crate) fn unlisten(&self, channel: C) {
+        if let Err(error) = self.listener_tx.send(Command::Unlisten(channel)) {
             error!(%error, "Error sending unlisten command to pg listener task");
         }
     }
@@ -59,7 +59,7 @@ pub(crate) async fn spawn_pg_listener_task<C: PgChannelName>(
 ) -> sqlx::Result<PgListenerTaskHandle<C>> {
     let (broadcast, _) = broadcast::channel(1024);
     let mut listener = PgListener::connect_with(&pool).await?;
-    let (listener_tx, mut listener_rx) = mpsc::channel(1024);
+    let (listener_tx, mut listener_rx) = mpsc::unbounded_channel();
 
     // Cancelled when listener_tx is dropped
     let broadcast_inner = broadcast.clone();
@@ -96,15 +96,20 @@ async fn handle_loop_event<C: PgChannelName>(
         LoopEvent::Notification(notification) => {
             let notification = notification?;
             let channel = notification.channel();
+            debug!(channel, "received notification");
             let channel = C::from_pg_channel(channel)
                 .ok_or_else(|| LoopError::InvalidChannel(channel.to_string()))?;
             broadcast.send(channel).ok();
         }
         LoopEvent::Command(Command::Listen(channel)) => {
-            listener.listen(&channel.pg_channel()).await?;
+            let pg_channel = channel.pg_channel();
+            debug!(pg_channel, "listen");
+            listener.listen(&pg_channel).await?;
         }
         LoopEvent::Command(Command::Unlisten(channel)) => {
-            listener.unlisten(&channel.pg_channel()).await?;
+            let pg_channel = channel.pg_channel();
+            debug!(pg_channel, "unlisten");
+            listener.unlisten(&pg_channel).await?;
         }
     }
     Ok(())

backend/src/qs/grpc.rs
@@ -54,16 +54,14 @@ impl GrpcQs {
                     && let Some(io_error) = h2_error.get_io()
                     && io_error.kind() == io::ErrorKind::BrokenPipe
                 {
-                    return; // Client closed connection => not an error
+                    // Client closed connection => not an error
+                } else {
+                    // We report the error, but don't stop processing requests.
+                    // TODO(#466): Send this to the client.
+                    error!(%error, "error processing listen queue request");
                 }
-
-                // We report the error, but don't stop processing requests.
-                // TODO(#466): Send this to the client.
-                error!(%error, "error processing listen queue request");
             }
         }
-        // Listening stream was closed
-        queues.stop_listening(queue_id).await;
     }
 
     async fn process_listen_queue_request(

backend/src/qs/queue.rs
@@ -15,7 +15,7 @@ use sqlx::{PgExecutor, PgPool, PgTransaction};
 use tokio::sync::mpsc;
 use tokio_stream::StreamExt;
 use tokio_util::sync::CancellationToken;
-use tracing::error;
+use tracing::{debug, error};
 use uuid::Uuid;
 
 use crate::{
@@ -52,7 +52,6 @@ impl ListenerContext {
 
 impl Drop for ListenerContext {
     fn drop(&mut self) {
-        gauge!(METRIC_AIR_ACTIVE_USERS).decrement(1);
         self.cancel.cancel();
     }
 }
@@ -78,6 +77,7 @@ impl Queues {
         let cancel = self.track_listener(queue_id, payload_tx).await?;
         let context = QueueStreamContext {
             pool: self.pool.clone(),
+            pg_listener_task_handle: self.pg_listener_task_handle.clone(),
             notifications,
             queue_id,
             sequence_number: sequence_number_start,
@@ -161,37 +161,18 @@ impl Queues {
         client_id: QsClientId,
         payload_tx: mpsc::Sender<QueueEventPayload>,
     ) -> sqlx::Result<CancellationToken> {
-        self.clean_up_cancelled_listeners().await;
+        // Clean up cancelled listeners
+        self.listeners
+            .retain(|_id, context| !context.cancel.is_cancelled());
 
         let cancel = CancellationToken::new();
         let context = ListenerContext::new(cancel.clone(), payload_tx);
 
         if self.listeners.insert(client_id, context).is_none() {
-            self.pg_listener_task_handle.listen(client_id).await;
+            self.pg_listener_task_handle.listen(client_id);
         }
 
         Ok(cancel)
-    }
-
-    async fn clean_up_cancelled_listeners(&self) {
-        let mut removed_ids = Vec::new();
-        self.listeners.retain(|id, context| {
-            if context.cancel.is_cancelled() {
-                removed_ids.push(*id);
-                false // remove
-            } else {
-                true // keep
-            }
-        });
-        for id in removed_ids {
-            self.pg_listener_task_handle.unlisten(id).await;
-        }
-    }
-
-    pub(crate) async fn stop_listening(&self, queue_id: QsClientId) {
-        if let Some((_, context)) = self.listeners.remove(&queue_id) {
-            context.cancel.cancel();
-        }
     }
 }
 
@@ -208,6 +189,7 @@ impl PgChannelName for QsClientId {
 
 struct QueueStreamContext<S> {
     pool: PgPool,
+    pg_listener_task_handle: PgListenerTaskHandle<QsClientId>,
     notifications: S,
     queue_id: QsClientId,
     sequence_number: u64,
@@ -217,6 +199,15 @@ struct QueueStreamContext<S> {
     /// Invariant: the messages are stored in ascending order by sequence number.
     buffer: VecDeque<QueueMessage>,
     state: FetchState,
+}
+
+impl<S> Drop for QueueStreamContext<S> {
+    fn drop(&mut self) {
+        self.cancel.cancel();
+        self.pg_listener_task_handle.unlisten(self.queue_id);
+        gauge!(METRIC_AIR_ACTIVE_USERS).decrement(1);
+        debug!(queue_id =? self.queue_id, "QS queue stream stopped");
+    }
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -235,6 +226,8 @@ impl<S: Stream<Item = ()> + Send + Unpin> QueueStreamContext<S> {
     fn into_stream(self) -> impl Stream<Item = Option<QueueMessage>> + Send {
         stream::unfold(
             self,
+            // Note: This function must be cancellation safe, because the stream can be dropped any
+            // time, when the client disconnects.
             async |mut context| -> Option<(Option<QueueMessage>, Self)> {
                 loop {
                     if let Some(message) = context.buffer.pop_front() {