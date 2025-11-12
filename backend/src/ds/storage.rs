e706d90a118493271692547a32302332a9a406b3
boxdot <dimitri@phnx.im>
2025-11-12 12:20:00 +0100

    feat: add server configuration to enable/disable post policy uploads (#812)
    By default, the post policy uploads are disabled.

--- backend/src/ds/storage.rs
+++ backend/src/ds/storage.rs
@@ -6,64 +6,45 @@ use std::sync::Arc;
 
 use aws_config::Region;
 use aws_sdk_s3::{Client, Config, config::Credentials};
-use chrono::Duration;
 
-use crate::settings::{SecretAccessKey, StorageSettings};
+use crate::settings::StorageSettings;
 
 #[derive(Debug, Clone)]
 pub struct Storage {
     client: Client,
-    attributes: Arc<StorageAttributes>,
-}
-
-#[derive(Debug)]
-pub(crate) struct StorageAttributes {
-    pub(crate) endpoint_url: String,
-    pub(crate) access_key_id: String,
-    pub(crate) secret_access_key: SecretAccessKey,
-    pub(crate) region: String,
-    pub(crate) upload_expiration: Duration,
-    pub(crate) download_expiration: Duration,
-    pub(crate) max_attachment_size: u64,
+    settings: Arc<StorageSettings>,
 }
 
 impl Storage {
     pub fn new(settings: StorageSettings) -> Self {
-        let attributes = Arc::new(StorageAttributes {
-            endpoint_url: settings.endpoint.clone(),
-            access_key_id: settings.access_key_id.clone(),
-            secret_access_key: settings.secret_access_key.clone(),
-            region: settings.region.clone(),
-            upload_expiration: settings.upload_expiration,
-            download_expiration: settings.download_expiration,
-            max_attachment_size: settings.max_attachment_size,
-        });
-
         let credentials = Credentials::new(
-            settings.access_key_id,
-            settings.secret_access_key,
+            settings.access_key_id.clone(),
+            settings.secret_access_key.clone(),
             None,
             None,
             "storage",
         );
         let config = Config::builder()
-            .endpoint_url(settings.endpoint)
-            .region(Region::new(settings.region))
+            .endpoint_url(settings.endpoint.clone())
+            .region(Region::new(settings.region.clone()))
             .credentials_provider(credentials)
             .force_path_style(settings.force_path_style)
             .behavior_version_latest()
             .build();
 
-        let client = Client::from_conf(config.clone());
+        let client = Client::from_conf(config);
 
-        Self { client, attributes }
+        Self {
+            client,
+            settings: Arc::new(settings),
+        }
     }
 
     pub(crate) fn client(&self) -> Client {
         self.client.clone()
     }
 
-    pub(crate) fn attributes(&self) -> &StorageAttributes {
-        &self.attributes
+    pub(crate) fn settings(&self) -> &StorageSettings {
+        &self.settings
     }
 }
