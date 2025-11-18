ee9b97c58b880367521608163ebec454205384f2
raphaelrobert <git@raphaelrobert.com>
2025-11-18 20:47:17 +0100

    license & assets

--- app/lib/ui/components/desktop/width_constraints.dart
+++ app/lib/ui/components/desktop/width_constraints.dart
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Phoenix R&D GmbH <hello@phnx.im>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 import 'package:flutter/widgets.dart';
 
 /// A widget that constrains only the width of its child to 800 logical pixels.
