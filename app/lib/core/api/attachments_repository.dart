75d75302bca92d4a26d67c621c063ad5cbb2e094
boxdot <dimitri@phnx.im>
2025-11-25 12:56:51 +0100

    update frb

--- app/lib/core/api/attachments_repository.dart
+++ app/lib/core/api/attachments_repository.dart
@@ -14,7 +14,7 @@ import 'package:uuid/uuid.dart';
 import 'user_cubit.dart';
 part 'attachments_repository.freezed.dart';
 
-// These functions are ignored because they are not marked as `pub`: `attachment_downloads_loop`, `cancellation_token`, `in_progress`, `is_cancelled`, `new`, `spawn_attachment_downloads`, `spawn_download_task`, `track_attachment_download`, `with_cancellation`
+// These functions are ignored because they are not marked as `pub`: `attachment_downloads_loop`, `cancellation_token`, `in_progress`, `is_cancelled`, `new`, `spawn_attachment_downloads`, `spawn_download_task`, `track_attachment_download`, `unique_path`, `with_cancellation`
 // These types are ignored because they are neither used by any `pub` functions nor (for structs and enums) marked `#[frb(unignore)]`: `AttachmentTaskHandle`
 // These function are ignored because they are on traits that is not defined in current crate (put an empty `#[frb]` on it to unignore): `clone`, `fmt`
 
