044618f8759a75b1682bdbc4ec225519bf961729
boxdot <dimitri@phnx.im>
2025-12-02 18:01:34 +0100

    add test showing the size in bytes of client metadata

--- protos/src/common/mod.rs
+++ protos/src/common/mod.rs
@@ -4,3 +4,22 @@
 
 pub mod convert;
 pub mod v1;
+
+#[cfg(test)]
+mod test {
+
+    use prost::Message;
+
+    use super::*;
+
+    #[test]
+    fn client_metadata_size_bytes() {
+        let metadata = v1::ClientMetadata {
+            version: "0.1.0+100".to_owned(),
+            platform: v1::Platform::Android.into(),
+            channel: v1::ReleaseChannel::Stable.into(),
+        };
+        let bytes = metadata.encode_to_vec();
+        assert_eq!(bytes.len(), 15);
+    }
+}
