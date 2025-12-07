19983cee3ccee21f35cd0f0e6c16546308987dc5
raphaelrobert <git@raphaelrobert.com>
2025-12-07 17:45:50 +0100

    adjust link color

--- app/lib/ui/colors/themes.dart
+++ app/lib/ui/colors/themes.dart
@@ -71,7 +71,6 @@ final CustomColorScheme lightCustomColorScheme = CustomColorScheme(
     warning: AppColors.yellow[400]!,
     danger: AppColors.red[400]!,
     link: AppColors.blue[400]!,
-    selfLink: AppColors.blue[150]!,
   ),
   message: MessageColors(
     selfBackground: AppColors.neutral[100]!,
@@ -140,7 +139,6 @@ final CustomColorScheme darkCustomColorScheme = CustomColorScheme(
     warning: AppColors.yellow[500]!,
     danger: AppColors.red[500]!,
     link: AppColors.blue[200]!,
-    selfLink: AppColors.blue[600]!,
   ),
   message: MessageColors(
     selfBackground: AppColors.neutral[900]!,
