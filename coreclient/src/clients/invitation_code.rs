3bd70959bf287b9fa578200221d739d7721c3f33
boxdot <dimitri@phnx.im>
2025-12-12 23:33:11 +0100

    hotfix(app): new users have invalid domain (#909)
    The bug was introduced in #900 where we started redirecting from air.ms
    to prod.air.ms in the registration cubit. This is however wrong, since
    then new users have prod.air.ms as domain, which is not valid.
    
    This fix removes the faulty redirection and does it only in the check
    validation code function.

--- coreclient/src/clients/invitation_code.rs
+++ coreclient/src/clients/invitation_code.rs
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 use anyhow::Context;
-use url::Url;
+use url::{Host, Url};
 
 use crate::clients::{CoreUser, api_clients::ApiClients};
 
@@ -13,10 +13,15 @@ impl CoreUser {
     /// Note: This function creates a new API client for each call. Therefore, the TCP/TLS/HTTP
     /// connection is not reused.
     pub async fn check_invitation_code(
-        server_url: Url,
+        mut server_url: Url,
         invitation_code: String,
     ) -> anyhow::Result<bool> {
-        let domain = server_url.domain().context("missing domain")?.parse()?;
+        let domain = server_url.host().context("missing domain")?.to_owned();
+        if domain == Host::Domain("air.ms") {
+            // TODO: For now, we redirect to the production server manually.
+            server_url.set_host(Some("prod.air.ms"))?;
+        }
+        let domain = domain.to_owned().into();
         let api_clients = ApiClients::new(domain, server_url.clone());
         let api_client = api_clients.default_client()?;
         Ok(api_client.as_check_invitation_code(invitation_code).await?)

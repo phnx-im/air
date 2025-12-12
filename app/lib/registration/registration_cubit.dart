phnx-im/air

commit 408f09d35c54d61c0fd6dcf95ecbc3b26cc8ee61

fix(app): redirect to prod server for checking invitation code (#900)
Also fix unredeemable code in the server config.

app/lib/registration/registration_cubit.dart
@@ -40,8 +40,12 @@ sealed class RegistrationState with _$RegistrationState {
   bool get isDomainValid => _domainRegex.hasMatch(domain);
   bool get isValid =>
       isDomainValid && displayName.trim().isNotEmpty && invitationCode != null;
-  String get serverUrl =>
-      domain == "localhost" ? "http://$domain:8080" : "https://$domain";
+  String get serverUrl => domain == "localhost"
+      ? "http://$domain:8080"
+      : domain == "air.ms"
+      // For now, we redirect to the production server manually.
+      ? "https://prod.air.ms"
+      : "https://$domain";
 }
 
 class RegistrationCubit extends Cubit<RegistrationState> {
@@ -114,14 +118,10 @@ class RegistrationCubit extends Cubit<RegistrationState> {
 
     emit(state.copyWith(isSigningUp: true));
 
-    final url = state.domain == "localhost"
-        ? "http://${state.domain}:8080"
-        : "https://${state.domain}";
-
     try {
       _log.info("Registering user...");
       await _coreClient.createUser(
-        url,
+        state.serverUrl,
         state.displayName,
         state.avatar?.data,
         state.invitationCode!,

server/src/main.rs
@@ -112,13 +112,16 @@ async fn main() -> anyhow::Result<()> {
 
     // New database name for the AS provider
     configuration.database.name = format!("{base_db_name}_as");
-    let auth_service = AuthService::new(
+    let mut auth_service = AuthService::new(
         &configuration.database,
         domain.clone(),
         version_req.cloned(),
     )
     .await
     .expect("Failed to connect to database.");
+    if let Some(code) = configuration.application.unredeemablecode {
+        auth_service.set_unredeemable_code(code);
+    }
 
     let push_notification_provider =
         ProductionPushNotificationProvider::new(configuration.fcm, configuration.apns)?;

commit 3bd70959bf287b9fa578200221d739d7721c3f33

hotfix(app): new users have invalid domain (#909)
The bug was introduced in #900 where we started redirecting from air.ms
to prod.air.ms in the registration cubit. This is however wrong, since
then new users have prod.air.ms as domain, which is not valid.

This fix removes the faulty redirection and does it only in the check
validation code function.

app/lib/registration/registration_cubit.dart
@@ -40,12 +40,8 @@ sealed class RegistrationState with _$RegistrationState {
   bool get isDomainValid => _domainRegex.hasMatch(domain);
   bool get isValid =>
       isDomainValid && displayName.trim().isNotEmpty && invitationCode != null;
-  String get serverUrl => domain == "localhost"
-      ? "http://$domain:8080"
-      : domain == "air.ms"
-      // For now, we redirect to the production server manually.
-      ? "https://prod.air.ms"
-      : "https://$domain";
+  String get serverUrl =>
+      domain == "localhost" ? "http://$domain:8080" : "https://$domain";
 }
 
 class RegistrationCubit extends Cubit<RegistrationState> {

coreclient/src/clients/invitation_code.rs
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 use anyhow::Context;
-use url::Url;
+use url::{Host, Url};
 
 use crate::clients::{CoreUser, api_clients::ApiClients};
 
@@ -13,10 +13,15 @@ impl CoreUser {
     /// Note: This function creates a new API client for each call. Therefore, the TCP/TLS/HTTP
     /// connection is not reused.
     pub async fn check_invitation_code(
-        server_url: Url,
+        mut server_url: Url,
         invitation_code: String,
     ) -> anyhow::Result<bool> {
-        let domain = server_url.domain().context("missing domain")?.parse()?;
+        let domain = server_url.host().context("missing domain")?.to_owned();
+        if domain == Host::Domain("air.ms") {
+            // TODO: For now, we redirect to the production server manually.
+            server_url.set_host(Some("prod.air.ms"))?;
+        }
+        let domain = domain.to_owned().into();
         let api_clients = ApiClients::new(domain, server_url.clone());
         let api_client = api_clients.default_client()?;
         Ok(api_client.as_check_invitation_code(invitation_code).await?)