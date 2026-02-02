phnx-im/air

commit ce5f0f8be9c95ef21696b53a15ed84e377dceada

feat(app): surface attachment size limit on upload (#974)
Errors are surfaced to UI via anyhow::Result<Result<T, E>>. The outer
error is considered fatal. UI shows a banner with an error message that
something went wrong and logs the actual error as fatal. The inner error
is transformed into an option and sent to UI. UI must handle the option
and show the error message to the user.

app/lib/chat_list/add_contact_dialog.dart
@@ -265,33 +265,27 @@ class _SubmitHandler {
     final loc = AppLocalizations.of(context);
     final chatListCubit = context.read<ChatListCubit>();
     try {
-      final result = await chatListCubit.createContactChat(
+      final error = await chatListCubit.createContactChat(
         handle: handle,
         hash: hash,
       );
-      switch (result) {
-        case AddHandleContactResult_Ok():
-          // success
-          if (context.mounted) {
-            Navigator.of(context).pop();
-          }
-        case AddHandleContactResult_Err(field0: final error):
-          // error
-          final errorMessage = switch (error) {
-            AddHandleContactError.handleNotFound =>
-              loc.newConnectionDialog_error_handleNotFound(handle.plaintext),
-            AddHandleContactError.duplicateRequest =>
-              loc.newConnectionDialog_error_duplicateRequest,
-            AddHandleContactError.ownHandle =>
-              loc.newConnectionDialog_error_ownHandle,
-          };
-          this.errorMessage.value = errorMessage;
+      final errorMessage = switch (error) {
+        AddHandleContactError.handleNotFound =>
+          loc.newConnectionDialog_error_handleNotFound(handle.plaintext),
+        AddHandleContactError.duplicateRequest =>
+          loc.newConnectionDialog_error_duplicateRequest,
+        AddHandleContactError.ownHandle =>
+          loc.newConnectionDialog_error_ownHandle,
+        null => null,
+      };
+      if (errorMessage != null) {
+        this.errorMessage.value = errorMessage;
       }
     } catch (e) {
       // fatal error
       Logger.detached(
         "AddContactDialog",
-      ).severe("Failed to create connection: $e");
+      ).severe("Failed to create connection: $e", e);
       showErrorBannerStandalone(
         (loc) => loc.newConnectionDialog_error(handle.plaintext),
       );

apiclient/src/ds_api.rs
@@ -21,6 +21,7 @@ use aircommon::{
 };
 pub use airprotos::delivery_service::v1::ProvisionAttachmentResponse;
 use airprotos::{
+    common::v1::{AttachmentTooLargeDetail, StatusDetails, status_details::Detail},
     convert::{RefInto, TryRefInto},
     delivery_service::v1::{
         AddUsersInfo, ConnectionGroupInfoRequest, CreateGroupPayload, DeleteGroupPayload,
@@ -36,6 +37,7 @@ use mls_assist::{
     messages::AssistedMessageOut,
     openmls::prelude::{GroupEpoch, GroupId, LeafNodeIndex, MlsMessageOut},
 };
+use tonic::Code;
 use tracing::error;
 
 use crate::ApiClient;
@@ -56,6 +58,23 @@ pub enum DsRequestError {
 impl From<LibraryError> for DsRequestError {
     fn from(_: LibraryError) -> Self {
         Self::LibraryError
+    }
+}
+
+impl DsRequestError {
+    pub fn get_attachment_too_large(&self) -> Option<AttachmentTooLargeDetail> {
+        if let Self::Tonic(status) = self
+            && status.code() == Code::InvalidArgument
+        {
+            let details = StatusDetails::from_status(status)?;
+            if let Detail::AttachmentTooLarge(attachment_too_large_detail) = details.detail? {
+                Some(attachment_too_large_detail)
+            } else {
+                None
+            }
+        } else {
+            None
+        }
     }
 }
 

app/lib/chat/chat_details_cubit.dart
@@ -57,11 +57,12 @@ class ChatDetailsCubit extends StateStreamableSource<ChatDetailsState> {
 
   Future<void> deleteMessage() => _impl.deleteMessage();
 
-  Future<void> uploadAttachment(String path) =>
+  Future<UploadAttachmentError?> uploadAttachment(String path) =>
       _impl.uploadAttachment(path: path);
 
-  Future<void> retryUploadAttachment(AttachmentId attachmentId) =>
-      _impl.retryUploadAttachment(attachmentId: attachmentId);
+  Future<UploadAttachmentError?> retryUploadAttachment(
+    AttachmentId attachmentId,
+  ) => _impl.retryUploadAttachment(attachmentId: attachmentId);
 
   Future<void> markAsRead({
     required MessageId untilMessageId,

app/lib/chat_list/chat_list_cubit.dart
@@ -29,7 +29,7 @@ class ChatListCubit implements StateStreamableSource<ChatListState> {
   @override
   Stream<ChatListState> get stream => _impl.stream();
 
-  Future<AddHandleContactResult> createContactChat({
+  Future<AddHandleContactError?> createContactChat({
     required UiUserHandle handle,
     required UserHandleHash hash,
   }) => _impl.createContactChat(handle: handle, hash: hash);

app/lib/core/api/chat_details_cubit.dart
@@ -22,6 +22,8 @@ part 'chat_details_cubit.freezed.dart';
 // These functions are ignored because they are not marked as `pub`: `load_and_emit_state`, `load_chat_details`, `load_chat_details`, `new`, `store_draft_from_state`, `update_state_task`, `upload_attachment_impl`
 // These types are ignored because they are neither used by any `pub` functions nor (for structs and enums) marked `#[frb(unignore)]`: `ChatDetailsContext`
 // These function are ignored because they are on traits that is not defined in current crate (put an empty `#[frb]` on it to unignore): `assert_receiver_is_total_eq`, `clone`, `clone`, `eq`, `fmt`, `hash`
+// These functions are ignored (category: IgnoreBecauseExplicitAttribute): `into_ui_result`
+// These functions are ignored (category: IgnoreBecauseNotAllowedOwner): `into_ui_result`
 
 // Rust type: RustOpaqueMoi<flutter_rust_bridge::for_generated::RustAutoOpaqueInner<ChatDetailsCubitBase>>
 abstract class ChatDetailsCubitBase implements RustOpaqueInterface {
@@ -65,7 +67,9 @@ abstract class ChatDetailsCubitBase implements RustOpaqueInterface {
 
   Future<void> resetDraft();
 
-  Future<void> retryUploadAttachment({required AttachmentId attachmentId});
+  Future<UploadAttachmentError?> retryUploadAttachment({
+    required AttachmentId attachmentId,
+  });
 
   /// Sends a message to the chat.
   ///
@@ -89,7 +93,7 @@ abstract class ChatDetailsCubitBase implements RustOpaqueInterface {
 
   Stream<ChatDetailsState> stream();
 
-  Future<void> uploadAttachment({required String path});
+  Future<UploadAttachmentError?> uploadAttachment({required String path});
 }
 
 /// The state of a single chat
@@ -106,4 +110,14 @@ sealed class ChatDetailsState with _$ChatDetailsState {
   }) = _ChatDetailsState;
   static Future<ChatDetailsState> default_() =>
       RustLib.instance.api.crateApiChatDetailsCubitChatDetailsStateDefault();
+}
+
+@freezed
+sealed class UploadAttachmentError with _$UploadAttachmentError {
+  const UploadAttachmentError._();
+
+  const factory UploadAttachmentError.tooLarge({
+    required BigInt maxSizeBytes,
+    required BigInt actualSizeBytes,
+  }) = UploadAttachmentError_TooLarge;
 }

app/lib/core/api/chat_details_cubit.freezed.dart
@@ -148,4 +148,135 @@ as List<UiUserId>,
 
 }
 
+/// @nodoc
+mixin _$UploadAttachmentError {
+
+ BigInt get maxSizeBytes; BigInt get actualSizeBytes;
+/// Create a copy of UploadAttachmentError
+/// with the given fields replaced by the non-null parameter values.
+@JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+$UploadAttachmentErrorCopyWith<UploadAttachmentError> get copyWith => _$UploadAttachmentErrorCopyWithImpl<UploadAttachmentError>(this as UploadAttachmentError, _$identity);
+
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is UploadAttachmentError&&(identical(other.maxSizeBytes, maxSizeBytes) || other.maxSizeBytes == maxSizeBytes)&&(identical(other.actualSizeBytes, actualSizeBytes) || other.actualSizeBytes == actualSizeBytes));
+}
+
+
+@override
+int get hashCode => Object.hash(runtimeType,maxSizeBytes,actualSizeBytes);
+
+@override
+String toString() {
+  return 'UploadAttachmentError(maxSizeBytes: $maxSizeBytes, actualSizeBytes: $actualSizeBytes)';
+}
+
+
+}
+
+/// @nodoc
+abstract mixin class $UploadAttachmentErrorCopyWith<$Res>  {
+  factory $UploadAttachmentErrorCopyWith(UploadAttachmentError value, $Res Function(UploadAttachmentError) _then) = _$UploadAttachmentErrorCopyWithImpl;
+@useResult
+$Res call({
+ BigInt maxSizeBytes, BigInt actualSizeBytes
+});
+
+
+
+
+}
+/// @nodoc
+class _$UploadAttachmentErrorCopyWithImpl<$Res>
+    implements $UploadAttachmentErrorCopyWith<$Res> {
+  _$UploadAttachmentErrorCopyWithImpl(this._self, this._then);
+
+  final UploadAttachmentError _self;
+  final $Res Function(UploadAttachmentError) _then;
+
+/// Create a copy of UploadAttachmentError
+/// with the given fields replaced by the non-null parameter values.
+@pragma('vm:prefer-inline') @override $Res call({Object? maxSizeBytes = null,Object? actualSizeBytes = null,}) {
+  return _then(_self.copyWith(
+maxSizeBytes: null == maxSizeBytes ? _self.maxSizeBytes : maxSizeBytes // ignore: cast_nullable_to_non_nullable
+as BigInt,actualSizeBytes: null == actualSizeBytes ? _self.actualSizeBytes : actualSizeBytes // ignore: cast_nullable_to_non_nullable
+as BigInt,
+  ));
+}
+
+}
+
+
+
+/// @nodoc
+
+
+class UploadAttachmentError_TooLarge extends UploadAttachmentError {
+  const UploadAttachmentError_TooLarge({required this.maxSizeBytes, required this.actualSizeBytes}): super._();
+  
+
+@override final  BigInt maxSizeBytes;
+@override final  BigInt actualSizeBytes;
+
+/// Create a copy of UploadAttachmentError
+/// with the given fields replaced by the non-null parameter values.
+@override @JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+$UploadAttachmentError_TooLargeCopyWith<UploadAttachmentError_TooLarge> get copyWith => _$UploadAttachmentError_TooLargeCopyWithImpl<UploadAttachmentError_TooLarge>(this, _$identity);
+
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is UploadAttachmentError_TooLarge&&(identical(other.maxSizeBytes, maxSizeBytes) || other.maxSizeBytes == maxSizeBytes)&&(identical(other.actualSizeBytes, actualSizeBytes) || other.actualSizeBytes == actualSizeBytes));
+}
+
+
+@override
+int get hashCode => Object.hash(runtimeType,maxSizeBytes,actualSizeBytes);
+
+@override
+String toString() {
+  return 'UploadAttachmentError.tooLarge(maxSizeBytes: $maxSizeBytes, actualSizeBytes: $actualSizeBytes)';
+}
+
+
+}
+
+/// @nodoc
+abstract mixin class $UploadAttachmentError_TooLargeCopyWith<$Res> implements $UploadAttachmentErrorCopyWith<$Res> {
+  factory $UploadAttachmentError_TooLargeCopyWith(UploadAttachmentError_TooLarge value, $Res Function(UploadAttachmentError_TooLarge) _then) = _$UploadAttachmentError_TooLargeCopyWithImpl;
+@override @useResult
+$Res call({
+ BigInt maxSizeBytes, BigInt actualSizeBytes
+});
+
+
+
+
+}
+/// @nodoc
+class _$UploadAttachmentError_TooLargeCopyWithImpl<$Res>
+    implements $UploadAttachmentError_TooLargeCopyWith<$Res> {
+  _$UploadAttachmentError_TooLargeCopyWithImpl(this._self, this._then);
+
+  final UploadAttachmentError_TooLarge _self;
+  final $Res Function(UploadAttachmentError_TooLarge) _then;
+
+/// Create a copy of UploadAttachmentError
+/// with the given fields replaced by the non-null parameter values.
+@override @pragma('vm:prefer-inline') $Res call({Object? maxSizeBytes = null,Object? actualSizeBytes = null,}) {
+  return _then(UploadAttachmentError_TooLarge(
+maxSizeBytes: null == maxSizeBytes ? _self.maxSizeBytes : maxSizeBytes // ignore: cast_nullable_to_non_nullable
+as BigInt,actualSizeBytes: null == actualSizeBytes ? _self.actualSizeBytes : actualSizeBytes // ignore: cast_nullable_to_non_nullable
+as BigInt,
+  ));
+}
+
+
+}
+
 // dart format on

app/lib/core/api/chat_list_cubit.dart
@@ -25,7 +25,7 @@ abstract class ChatListCubitBase implements RustOpaqueInterface {
   /// Creates a new 1:1 connection with the given user via a user handle.
   ///
   /// Returns `None` if the provided handle does not exist.
-  Future<AddHandleContactResult> createContactChat({
+  Future<AddHandleContactError?> createContactChat({
     required UiUserHandle handle,
     required UserHandleHash hash,
   });

app/lib/core/api/types.dart
@@ -19,17 +19,8 @@ part 'types.freezed.dart';
 // These function are ignored because they are on traits that is not defined in current crate (put an empty `#[frb]` on it to unignore): `assert_receiver_is_total_eq`, `assert_receiver_is_total_eq`, `assert_receiver_is_total_eq`, `assert_receiver_is_total_eq`, `assert_receiver_is_total_eq`, `assert_receiver_is_total_eq`, `assert_receiver_is_total_eq`, `assert_receiver_is_total_eq`, `assert_receiver_is_total_eq`, `assert_receiver_is_total_eq`, `assert_receiver_is_total_eq`, `assert_receiver_is_total_eq`, `assert_receiver_is_total_eq`, `assert_receiver_is_total_eq`, `assert_receiver_is_total_eq`, `assert_receiver_is_total_eq`, `assert_receiver_is_total_eq`, `assert_receiver_is_total_eq`, `assert_receiver_is_total_eq`, `assert_receiver_is_total_eq`, `clone`, `clone`, `clone`, `clone`, `clone`, `clone`, `clone`, `clone`, `clone`, `clone`, `clone`, `clone`, `clone`, `clone`, `clone`, `clone`, `clone`, `clone`, `clone`, `clone`, `cmp`, `eq`, `eq`, `eq`, `eq`, `eq`, `eq`, `eq`, `eq`, `eq`, `eq`, `eq`, `eq`, `eq`, `eq`, `eq`, `eq`, `eq`, `eq`, `eq`, `eq`, `fmt`, `fmt`, `fmt`, `fmt`, `fmt`, `fmt`, `fmt`, `fmt`, `fmt`, `fmt`, `fmt`, `fmt`, `fmt`, `fmt`, `fmt`, `fmt`, `fmt`, `fmt`, `fmt`, `fmt`, `fmt`, `from`, `from`, `from`, `from`, `from`, `from`, `from`, `from`, `from`, `from`, `from`, `from`, `from`, `hash`, `hash`, `hash`, `hash`, `hash`, `hash`, `hash`, `hash`, `hash`, `hash`, `hash`, `hash`, `hash`, `hash`, `hash`, `hash`, `hash`, `hash`, `hash`, `hash`, `partial_cmp`
 // These functions are ignored (category: IgnoreBecauseExplicitAttribute): `from`
 
+/// Mirror of the [`AddHandleContactError`] type
 enum AddHandleContactError { handleNotFound, duplicateRequest, ownHandle }
-
-@freezed
-sealed class AddHandleContactResult with _$AddHandleContactResult {
-  const AddHandleContactResult._();
-
-  const factory AddHandleContactResult.ok(ChatId field0) =
-      AddHandleContactResult_Ok;
-  const factory AddHandleContactResult.err(AddHandleContactError field0) =
-      AddHandleContactResult_Err;
-}
 
 /// Mirror of the [`ChatId`] type
 class ChatId {

commit 85b9a5ecaec71448576e6364cc829c54495333e6

fix(app): dismiss contact request dialog (#998)
Fixes a regression from #974 where the dialog for sending contact
requests is not closed after a request is sent.

app/lib/chat_list/add_contact_dialog.dart
@@ -280,6 +280,8 @@ class _SubmitHandler {
       };
       if (errorMessage != null) {
         this.errorMessage.value = errorMessage;
+      } else if (context.mounted) {
+        Navigator.of(context).pop();
       }
     } catch (e) {
       // fatal error