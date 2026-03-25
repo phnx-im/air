5a2ac71f4275a9eed56d6cfc90a0f3a71899ded3
Gabriel Féron <gabriel@phnx.im>
2026-03-25 14:37:24 +0100

    Move migration in correct directory

--- coreclient/migrations/20260325112913_pending_attachment_index.sql
+++ coreclient/migrations/20260325112913_pending_attachment_index.sql
@@ -1,3 +1,8 @@
+-- SPDX-FileCopyrightText: 2026 Phoenix R&D GmbH <hello@phnx.im>
+--
+-- SPDX-License-Identifier: AGPL-3.0-or-later
+--
+--
 -- Speeds up finding attachments that have not yet been downloaded
 CREATE INDEX idx_attachment_pending_ordered ON attachment (created_at)
 WHERE
