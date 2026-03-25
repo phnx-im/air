d8db813cf44fc7f286e9d2d5a98b56995e74968a
Gabriel Féron <gabriel@phnx.im>
2026-03-25 16:23:45 +0100

    Fix migration

--- coreclient/migrations/20260325112913_pending_attachment_index.sql
+++ coreclient/migrations/20260325112913_pending_attachment_index.sql
@@ -4,6 +4,6 @@
 --
 --
 -- Speeds up finding attachments that have not yet been downloaded
-CREATE INDEX idx_attachment_pending_ordered ON attachment (created_at)
+CREATE INDEX IF NOT EXISTS idx_attachment_pending_ordered ON attachment (created_at)
 WHERE
     status = 1;
