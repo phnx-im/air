b0489f88c85a8a83478a4b5090d3f50926075b3a
Gabriel Féron <gabriel@phnx.im>
2026-03-30 10:35:28 +0200

    fix(app): revamp UI scaling (specifically on Linux) (#1080)
    What happened on desktop was using the mobile base font size and
    depending on the desktop platform (Windows, macOS and Linux) the UI
    would be scaled according to a per-platform ratio of desired font size
    (overridden per platform) over the iOS base font size. On macOS we
    applied an extra tweak of 110% instead of boosting the desired font
    size.
    
    The way it was calculated resulted in a tiny UI on Linux, with text
    that's too big. With the current values, we ended up getting a factor of
    83% while the font sizes would stick to various sizes derived from 17pt.
    
    This PR aims to only fix UI scaling on Linux, by also scaling the UI
    when the system text-scale increases. This is, to the best of my
    knowledge, legacy behaviour that was required when X and Wayland
    co-existed, but is still what happens when tweaking this setting on
    software like Firefox or VSCode (or any Electron app).

--- app/lib/ui/theme/scale.dart
+++ app/lib/ui/theme/scale.dart
@@ -18,61 +18,48 @@ ScalingFactors getScalingFactors(BuildContext context) {
   const android = 16.0;
   const macos = 13.0;
   const windows = 15.0;
-  const linux = 14.0;
-
-  const iosUiTweak = 1.0;
-  const androidUiTweak = 1.0;
-  const macosUiTweak = 1.0;
-  const windowsUiTweak = 1.0;
-  const linuxUiTweak = 1.0;
-
-  const iosTextTweak = 1.0;
-  const androidTextTweak = 1.0;
-  const macosTextTweak = 1.1;
-  const windowsTextTweak = 1.0;
-  const linuxTextTweak = 1.0;
+  const linux = 15.0;
 
   const refBase = ios;
 
+  final mediaQuery = MediaQuery.of(context);
+  final systemTextScale = mediaQuery.textScaler.scale(1.0);
+
   if (Platform.isIOS) {
-    return const ScalingFactors(
-      uiFactor: ios / refBase * iosUiTweak,
-      textFactor: iosTextTweak,
-    );
+    return ScalingFactors(uiFactor: ios / refBase, textFactor: systemTextScale);
   } else if (Platform.isMacOS) {
-    return const ScalingFactors(
-      uiFactor: macos / refBase * macosUiTweak,
-      textFactor: macosTextTweak,
+    return ScalingFactors(
+      uiFactor: macos / refBase,
+      textFactor: 1.1 * systemTextScale,
     );
   } else if (Platform.isAndroid) {
-    const uiFactor = android / android * androidUiTweak;
+    const uiFactor = android / android;
     assert(
       uiFactor == 1.0,
       "Android UI scaling factor must be 1.0, "
       "in particular because of paddings to avoid system UI.",
     );
-    return const ScalingFactors(
+    return ScalingFactors(
       uiFactor: uiFactor,
-      textFactor: androidTextTweak,
+      textFactor: 1.0 * systemTextScale,
     );
   } else if (Platform.isWindows) {
-    return const ScalingFactors(
-      uiFactor: windows / refBase * windowsUiTweak,
-      textFactor: windowsTextTweak,
+    return ScalingFactors(
+      uiFactor: windows / refBase,
+      textFactor: 1.0 * systemTextScale,
     );
   } else if (Platform.isLinux) {
-    return const ScalingFactors(
-      uiFactor: linux / refBase * linuxUiTweak,
-      textFactor: linuxTextTweak,
+    // On Linux, we never manually scale the text only to behave like other apps
+    // like Firefox. Historically, there was no fine control of UI scaling
+    // in GNOME for HiDPI, which is why there's today both UI scaling and
+    // (in GNOME Tweaks) the legacy text scale factor still in use by some.
+    return ScalingFactors(
+      uiFactor: linux / refBase * systemTextScale,
+      textFactor: 1.0,
     );
   } else {
-    return const ScalingFactors(uiFactor: 1.0, textFactor: 1.0);
+    return ScalingFactors(uiFactor: 1.0, textFactor: 1.0 * systemTextScale);
   }
-}
-
-double actualTextSize(double fontSize, BuildContext context) {
-  final scalingFactors = getScalingFactors(context);
-  return fontSize * scalingFactors.textFactor;
 }
 
 double actualUiSize(double size, BuildContext context) {
