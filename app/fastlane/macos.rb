0b04b6e5ae44c2a3dabb1c748a356adb7717a586
Gabriel Féron <gabriel@phnx.im>
2026-04-08 16:55:53 +0200

    Move every CI env setup actions into a single composite custom action

--- app/fastlane/macos.rb
+++ app/fastlane/macos.rb
@@ -86,10 +86,10 @@ platform :mac do
     setup_ci()
 
     # Install flutter dependencies
-    sh "fvm flutter pub get"
+    sh "just flutter pub get"
 
     # Build the app with flutter first to create the necessary ephemeral files
-    sh "fvm flutter build macos --config-only #{skip_signing ? '--debug' : '--release'} --build-number=#{build_number}"
+    sh "just flutter build macos --config-only #{skip_signing ? '--debug' : '--release'} --build-number=#{build_number}"
 
     # Install CocoaPods dependencies
     cocoapods(
