dd402f7235ae697f1d7dfa09a4639ca885d78b1c
raphaelrobert <git@raphaelrobert.com>
2026-04-09 19:52:14 +0200

    cleanup after group-not-found

--- coreclient/src/outbound_service/chat_messages.rs
+++ coreclient/src/outbound_service/chat_messages.rs
@@ -12,6 +12,7 @@ use tracing::debug;
 use tracing::warn;
 use uuid::Uuid;
 
+use crate::groups::handle_group_not_found_on_ds;
 use crate::job::pending_chat_operation::PendingChatOperation;
 use crate::outbound_service::resync::Resync;
 use crate::utils::connection_ext::ConnectionExt as _;
@@ -193,11 +194,23 @@ impl OutboundServiceContext {
             .await?;
 
         // send MLS message to DS
-        let ds_timestamp = self
+        let ds_timestamp = match self
             .api_clients
             .get(&chat.owner_domain())?
             .ds_send_message(params, self.signing_key(), &group_state_ear_key)
-            .await?;
+            .await
+        {
+            Ok(ts) => ts,
+            Err(ds_error) => {
+                if ds_error.is_not_found() {
+                    self.with_transaction_and_notifier(async |txn, notifier| {
+                        handle_group_not_found_on_ds(txn, notifier, chat.group_id()).await
+                    })
+                    .await?;
+                }
+                return Err(ds_error.into());
+            }
+        };
 
         // post-processing:
         self.with_transaction_and_notifier(async |txn, notifier| {
