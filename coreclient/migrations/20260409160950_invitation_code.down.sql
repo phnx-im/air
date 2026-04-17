ab06677cf98e0474701cf74f193f46b31df95235
boxdot <dimitri@phnx.im>
2026-04-17 10:43:13 +0200

    fix down migration

--- coreclient/migrations/20260409160950_invitation_code.down.sql
+++ coreclient/migrations/20260409160950_invitation_code.down.sql
@@ -2,14 +2,31 @@
 --
 -- SPDX-License-Identifier: AGPL-3.0-or-later
 --
-ALTER TABLE privacy_pass_token
-DROP COLUMN operation_type;
+-- Revert privacy_pass_token to original schema
+CREATE TABLE privacy_pass_token_old (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    token BLOB NOT NULL
+);
 
-ALTER TABLE privacy_pass_token
-DROP COLUMN created_at;
+INSERT INTO
+    privacy_pass_token_old (id, token)
+SELECT
+    id,
+    token
+FROM
+    privacy_pass_token;
 
-ALTER TABLE batched_token_key
-DROP COLUMN operation_type;
+DROP TABLE privacy_pass_token;
+
+ALTER TABLE privacy_pass_token_old
+RENAME TO privacy_pass_token;
+
+-- Revert batched_token_key to original schema (data is ephemeral, can be discarded)
+DROP TABLE batched_token_key;
+
+CREATE TABLE batched_token_key (
+    token_key_id INTEGER PRIMARY KEY,
+    public_key BLOB NOT NULL
+);
 
 DROP TABLE invitation_code;
-
