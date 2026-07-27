7e3d84b0c7c584cb42c621873141b7d37fb24a49
Konrad Kohbrok <konrad.kohbrok@datashrine.de>
2026-07-27 13:01:36 +0000

    chore(app,server): add SPDX headers to new migration files

--- backend/migrations/20260727000000_rename_user_credential.down.sql
+++ backend/migrations/20260727000000_rename_user_credential.down.sql
@@ -1,1 +1,5 @@
+-- SPDX-FileCopyrightText: 2026 Phoenix R&D GmbH <hello@phnx.im>
+--
+-- SPDX-License-Identifier: AGPL-3.0-or-later
+
 ALTER TYPE user_credential RENAME TO client_credential;
--- coreclient/migrations/20260727000000_rename_user_credential.sql
+++ coreclient/migrations/20260727000000_rename_user_credential.sql
@@ -1,3 +1,7 @@
+-- SPDX-FileCopyrightText: 2026 Phoenix R&D GmbH <hello@phnx.im>
+--
+-- SPDX-License-Identifier: AGPL-3.0-or-later
+
 ALTER TABLE client_credential RENAME TO user_credential;
 ALTER TABLE user_credential RENAME COLUMN client_credential TO user_credential;
 DROP INDEX idx_client_credential_user_id;
