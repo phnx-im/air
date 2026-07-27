a09772736797afe1499e393471123e3f2554bb77
Konrad Kohbrok <konrad.kohbrok@datashrine.de>
2026-07-27 14:10:48 +0000

    feat(app,server): add creator_user_credential to group creation payloads

--- apiclient/src/ds_api.rs
+++ apiclient/src/ds_api.rs
@@ -198,6 +198,7 @@ impl ApiClient {
                 room_state: Some(room_state.unverified().try_ref_into()?),
                 t_group_data: Some(t_group_data),
                 pq_group_data: Some(pq_group_data),
+                creator_user_credential: None,
             };
             let request = payload.sign(signing_key)?;
             self.ds_grpc_client().create_apq_group(request).await?;
@@ -211,6 +212,7 @@ impl ApiClient {
                 creator_client_reference: Some(creator_client_reference.into()),
                 group_info: Some(group_info.try_ref_into()?),
                 room_state: Some(room_state.unverified().try_ref_into()?),
+                creator_user_credential: None,
             };
             let request = payload.sign(signing_key)?;
             self.ds_grpc_client().create_group(request).await?;
