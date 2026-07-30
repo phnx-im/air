8dc0cdc6e76ff482242feab22ddee28a5a2c244b
boxdot <dimitri@phnx.im>
2026-07-30 17:33:00 +0200

    move migration to the current date

--- backend/migrations/20260730151840_qs_staged_key_package.up.sql
+++ backend/migrations/20260730151840_qs_staged_key_package.up.sql
@@ -22,7 +22,7 @@ CREATE INDEX qs_staged_key_package_batch_created_at ON qs_staged_key_package_bat
 CREATE TABLE qs_staged_key_package (
     id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     batch_id INTEGER NOT NULL,
-    key_package BYTEA NOT NULL, -- TLS blob; APQ: t_kp.tls || pq_kp.tls
+    key_package BYTEA NOT NULL,
     is_last_resort BOOLEAN NOT NULL,
     is_apq BOOLEAN NOT NULL,
     FOREIGN KEY (batch_id) REFERENCES qs_staged_key_package_batch (id) ON DELETE CASCADE
