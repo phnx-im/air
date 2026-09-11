1b0f089f7316d97c264a0e39749c2c939fd72a79
boxdot <dimitri@phnx.im>
2026-09-11 09:19:23 +0200

    UI

--- app/lib/core/frb_generated.dart
+++ app/lib/core/frb_generated.dart
@@ -293,6 +293,7 @@ abstract class RustLibApi extends BaseApi {
     required ChatsDataSource that,
     required UiUsername username,
     required UsernameHash hash,
+    required bool preferApq,
   });
 
   Future<ChatId> crateApiChatsDataSourceChatsDataSourceCreateGroupChat({
@@ -563,6 +564,7 @@ abstract class RustLibApi extends BaseApi {
     required UserCubitBase that,
     required ChatId chatId,
     required UiUserId userId,
+    required bool preferApq,
   });
 
   Future<bool> crateApiUserCubitUserCubitBaseAddUsername({
@@ -2518,6 +2520,7 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
     required ChatsDataSource that,
     required UiUsername username,
     required UsernameHash hash,
+    required bool preferApq,
   }) {
     return handler.executeNormal(
       NormalTask(
@@ -2532,6 +2535,7 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
             hash,
             serializer,
           );
+          sse_encode_bool(preferApq, serializer);
           pdeCallFfi(
             generalizedFrbRustBinding,
             serializer,
@@ -2546,7 +2550,7 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
         ),
         constMeta:
             kCrateApiChatsDataSourceChatsDataSourceCreateContactChatConstMeta,
-        argValues: [that, username, hash],
+        argValues: [that, username, hash, preferApq],
         apiImpl: this,
       ),
     );
@@ -2556,7 +2560,7 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
   get kCrateApiChatsDataSourceChatsDataSourceCreateContactChatConstMeta =>
       const TaskConstMeta(
         debugName: "ChatsDataSource_create_contact_chat",
-        argNames: ["that", "username", "hash"],
+        argNames: ["that", "username", "hash", "preferApq"],
       );
 
   @override
@@ -4756,6 +4760,7 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
     required UserCubitBase that,
     required ChatId chatId,
     required UiUserId userId,
+    required bool preferApq,
   }) {
     return handler.executeNormal(
       NormalTask(
@@ -4767,6 +4772,7 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
           );
           sse_encode_box_autoadd_chat_id(chatId, serializer);
           sse_encode_box_autoadd_ui_user_id(userId, serializer);
+          sse_encode_bool(preferApq, serializer);
           pdeCallFfi(
             generalizedFrbRustBinding,
             serializer,
@@ -4779,7 +4785,7 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
           decodeErrorData: sse_decode_AnyhowException,
         ),
         constMeta: kCrateApiUserCubitUserCubitBaseAddContactFromGroupConstMeta,
-        argValues: [that, chatId, userId],
+        argValues: [that, chatId, userId, preferApq],
         apiImpl: this,
       ),
     );
@@ -4789,7 +4795,7 @@ class RustLibApiImpl extends RustLibApiImplPlatform implements RustLibApi {
   get kCrateApiUserCubitUserCubitBaseAddContactFromGroupConstMeta =>
       const TaskConstMeta(
         debugName: "UserCubitBase_add_contact_from_group",
-        argNames: ["that", "chatId", "userId"],
+        argNames: ["that", "chatId", "userId", "preferApq"],
       );
 
   @override
@@ -20288,11 +20294,13 @@ class ChatsDataSourceImpl extends RustOpaque implements ChatsDataSource {
   Future<AddUsernameContactError?> createContactChat({
     required UiUsername username,
     required UsernameHash hash,
+    required bool preferApq,
   }) => RustLib.instance.api
       .crateApiChatsDataSourceChatsDataSourceCreateContactChat(
         that: this,
         username: username,
         hash: hash,
+        preferApq: preferApq,
       );
 
   Future<ChatId> createGroupChat({
@@ -20901,10 +20909,12 @@ class UserCubitBaseImpl extends RustOpaque implements UserCubitBase {
   Future<ChatId> addContactFromGroup({
     required ChatId chatId,
     required UiUserId userId,
+    required bool preferApq,
   }) => RustLib.instance.api.crateApiUserCubitUserCubitBaseAddContactFromGroup(
     that: this,
     chatId: chatId,
     userId: userId,
+    preferApq: preferApq,
   );
 
   Future<bool> addUsername({required UiUsername username}) =>
--- applogic/src/frb_generated.rs
+++ applogic/src/frb_generated.rs
@@ -1753,6 +1753,7 @@ fn wire__crate__api__chats_data_source__ChatsDataSource_create_contact_chat_impl
             >>::sse_decode(&mut deserializer);
             let api_username = <crate::api::types::UiUsername>::sse_decode(&mut deserializer);
             let api_hash = <UsernameHash>::sse_decode(&mut deserializer);
+            let api_prefer_apq = <bool>::sse_decode(&mut deserializer);
             deserializer.end();
             move |context| async move {
                 transform_result_sse::<_, flutter_rust_bridge::for_generated::anyhow::Error>(
@@ -1779,6 +1780,7 @@ fn wire__crate__api__chats_data_source__ChatsDataSource_create_contact_chat_impl
                                 &*api_that_guard,
                                 api_username,
                                 api_hash,
+                                api_prefer_apq,
                             )
                             .await?;
                         std::result::Result::Ok(output_ok)
@@ -4890,6 +4892,7 @@ fn wire__crate__api__user_cubit__UserCubitBase_add_contact_from_group_impl(
             >>::sse_decode(&mut deserializer);
             let api_chat_id = <crate::api::types::ChatId>::sse_decode(&mut deserializer);
             let api_user_id = <crate::api::types::UiUserId>::sse_decode(&mut deserializer);
+            let api_prefer_apq = <bool>::sse_decode(&mut deserializer);
             deserializer.end();
             move |context| async move {
                 transform_result_sse::<_, flutter_rust_bridge::for_generated::anyhow::Error>(
@@ -4916,6 +4919,7 @@ fn wire__crate__api__user_cubit__UserCubitBase_add_contact_from_group_impl(
                                 &*api_that_guard,
                                 api_chat_id,
                                 api_user_id,
+                                api_prefer_apq,
                             )
                             .await?;
                         std::result::Result::Ok(output_ok)
